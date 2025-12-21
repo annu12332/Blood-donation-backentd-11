17ccab46f1781af4783fecb88e6f430ce46c588b
md anas <anas2542wy@gmail.com>
2025-12-21 15:29:53 +0600

    updated profile edit features

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection URI
 const uri = process.env.DB_URI || "mongodb+srv://Assignment-11:zBa83OMG66J7S6Mp@cluster0.buxlnsp.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
@@ -26,9 +24,9 @@ async function run() {
     const db = client.db("bloodDonationDB");
     const userCollection = db.collection("users");
     const donationCollection = db.collection("donationRequests");
-    const blogCollection = db.collection("blogs"); 
-   
-  
+    const blogCollection = db.collection("blogs");
+
+    // --- User Related APIs ---
     app.get('/users', async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
@@ -69,12 +67,18 @@ async function run() {
       res.send(result);
     });
 
-
-    // --- DONATION REQUESTS API ---
-
+    // --- Donation Request APIs ---
     app.post('/donation-requests', async (req, res) => {
       const request = req.body;
       const result = await donationCollection.insertOne(request);
+      res.send(result);
+    });
+
+    // এই রুটটি আমি শুধু অ্যাড করলাম আপনার ডিটেইলস পেজের জন্য
+    app.get('/donation-request-details/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donationCollection.findOne(query);
       res.send(result);
     });
 
@@ -92,28 +96,15 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/donation-request-details/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await donationCollection.findOne(query);
+    app.get('/all-donation-requests', async (req, res) => {
+      const result = await donationCollection.find().toArray();
       res.send(result);
     });
 
     app.patch('/donation-requests/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          recipientName: req.body.recipientName,
-          recipientDistrict: req.body.recipientDistrict,
-          recipientUpazila: req.body.recipientUpazila,
-          hospitalName: req.body.hospitalName,
-          fullAddress: req.body.fullAddress,
-          donationDate: req.body.donationDate,
-          donationTime: req.body.donationTime,
-          requestMessage: req.body.requestMessage,
-        }
-      };
+      const updatedDoc = { $set: { ...req.body } };
       const result = await donationCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
@@ -125,16 +116,14 @@ async function run() {
       res.send(result);
     });
 
-
-    // --- BLOG RELATED API ---
-
+    // --- BLOG APIs ---
     app.post('/blogs', async (req, res) => {
       const blog = req.body;
       const result = await blogCollection.insertOne(blog);
       res.send(result);
     });
 
-    app.get('/blogs', async (req, res) => {
+    app.get('/all-blogs', async (req, res) => {
       const result = await blogCollection.find().toArray();
       res.send(result);
     });
@@ -148,72 +137,57 @@ async function run() {
       res.send(result);
     });
 
-   
-    app.patch('/donation-requests/donate/:id', async (req, res) => {
+    app.delete('/blogs/:id', async (req, res) => {
       const id = req.params.id;
-      const { donorName, donorEmail, status } = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          donorName: donorName,
-          donorEmail: donorEmail,
-          donationStatus: status 
-        }
-      };
-      const result = await donationCollection.updateOne(filter, updatedDoc);
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.get('/donation-request-details/:id', async (req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-    const result = await donationCollection.findOne(query);
-    res.send(result);
-});
+    // --- Admin Stats ---
+    app.get('/admin-stats', async (req, res) => {
+      const usersCount = await userCollection.estimatedDocumentCount();
+      const requestsCount = await donationCollection.estimatedDocumentCount();
+      const successfulDonations = await donationCollection.countDocuments({ status: 'done' });
+      res.send({
+        users: usersCount,
+        requests: requestsCount,
+        doneDonations: successfulDonations
+      });
+    });
 
 
-app.get('/admin-stats', async (req, res) => {
-    try {
-        const usersCount = await userCollection.estimatedDocumentCount();
-        const requestsCount = await donationCollection.estimatedDocumentCount();
-        
-        
-        const successfulDonations = await donationCollection.countDocuments({ 
-            donationStatus: 'done' 
-        });
-
-        res.send({
-            users: usersCount,
-            requests: requestsCount,
-            doneDonations: successfulDonations
-        });
-    } catch (error) {
-        res.status(500).send({ message: "Error fetching stats" });
+   
+app.get('/users/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const result = await userCollection.findOne(query);
+    
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404).send({ message: "User not found" });
     }
 });
 
-
-
-    // --- SEARCH DONORS API ---
-
+    // --- Search Donors ---
     app.get('/search-donors', async (req, res) => {
       const { bloodGroup, district, upazila } = req.query;
       let query = { role: 'donor' };
-
-      
-      if (bloodGroup && bloodGroup !== "") query.bloodGroup = bloodGroup;
-      if (district && district !== "") query.district = district;
-      if (upazila && upazila !== "") query.upazila = upazila;
-
+      if (bloodGroup) query.bloodGroup = bloodGroup;
+      if (district) query.district = district;
+      if (upazila) query.upazila = upazila;
       const result = await userCollection.find(query).toArray();
       res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. Connected to MongoDB!");
-  } finally { }
+  } finally {
+    // Keep connection open
+  }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => { res.send("Blood Donation Server is running"); });
-app.listen(port, () => { console.log(`Server is running on ${port}`); });
+app.listen(port, () => { console.log(`Server is running on port ${port}`); });
